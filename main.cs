825f3ae2a71d206c840cd11f6fd07c41c82d78e6
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-05-04 01:49:25 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,31 @@
+// Created by: Johanna Liu
+// Created on: Mar 2022
+//
+// This program does basic math
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var days;
+        var age;
+
+        //Input 
+        Console.WriteLine("This program checks users eligibility for movies.");
+        Console.WriteLine("");
+        Console.Write("Enter day of the week: ");
+        days = Console.ReadLine();
+        Console.WriteLine("");
+        Console.Write("Enter age: ");
+        age = Convert.ToInt32(Console.ReadLine());
+
+        //Process
+        if ((days == "Tuesday" || days == "Thursday") || (age > 12 && age < 21))
+            Console.WriteLine("You are eligible for student pricing");
+        else
+            Console.WriteLine("You must pay regular price");
+
+        Console.WriteLine("\nDone.");
+    }
+}
